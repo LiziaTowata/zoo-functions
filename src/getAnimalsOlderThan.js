156e9c346bf1afa8f92448553f061a4e17dc05ec
Lizia Towata <liziatowata@gmail.com>
2022-07-29 18:04:38 -0300

    concluido requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const elemntAnimal = data.species
+   .find((elemnt) => elemnt.name === animal);
+//  return elemntAnimal.species
+  return elemntAnimal.residents.every((elemnt) => elemnt.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,6 +2,7 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
   // seu código aqui
+const animal = data.filter ((elemnt) => {})
 }
 
 module.exports = getSpeciesByIds;
