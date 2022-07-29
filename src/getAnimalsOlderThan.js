6098969920372730e1af0d0363b16bbf55c54911
Lizia Towata <liziatowata@gmail.com>
2022-07-29 18:13:05 -0300

    arrumando o lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,12 +1,9 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
   const elemntAnimal = data.species
-   .find((elemnt) => elemnt.name === animal);
-//  return elemntAnimal.species
+    .find((elemnt) => elemnt.name === animal);
   return elemntAnimal.residents.every((elemnt) => elemnt.age >= age);
 }
-
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,5 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
   // seu código aqui
-const animal = data.filter ((elemnt) => {})
 }
-
 module.exports = getSpeciesByIds;
