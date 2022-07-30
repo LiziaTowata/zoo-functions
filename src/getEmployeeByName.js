b914b76bbb7b5a9b8bdcb39ae80bf59fc8b6a0b8
Lizia Towata <liziatowata@gmail.com>
2022-07-29 23:31:54 -0300

    concluido requisito 6

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  // seu código aqui\
+
 }
 
 module.exports = getEmployeeByName;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,27 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('retorne a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+
+  it('retorne um array com a relacao dos nomes de todos os elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+
+  it('retorne a media de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+
+  it('retorne a localizacao dos elefantes dentro do Zoologico', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+
+  it('retorne a inteligencia dos elefantes', () => {
+    expect(handlerElephants('popularity')).toEqual(5);
+  });
+
+  it('retorne um array com relacao de dias em que e possivel visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+});
