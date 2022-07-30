d9244905f3fd2b58324363742b1fd78867f482fa
Lizia Towata <liziatowata@gmail.com>
2022-07-30 18:46:34 -0300

    arrumando o lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,11 +2,7 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
-  if (getEmployeeByName !== {}) {
-    return {};
-  }
-  const 'animalNames' = data.employees
-  .find((element) => element.firstName === employeeName || element.lastName === employeeName);  
+
 }
 
 module.exports = getEmployeeByName;
