76ed7436c720acaf4c6f45b3523e484299960d2f
Lizia Towata <liziatowata@gmail.com>
2022-07-31 18:55:51 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-
+  if (!ids) {
+    return [];
+  }
+  if (ids.length > 1) {
+    const moreId = data.species.filter((element) => ids.includes(element.id));
+    return moreId;
+  }
+  const justId = data.species.filter((element) => ids.includes(element.id));
+  return justId;
 }
 module.exports = getSpeciesByIds;
