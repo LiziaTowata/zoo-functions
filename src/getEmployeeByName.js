34d6f9af1a3b6e521d6bfc63f3703ed08bf51a75
Lizia Towata <liziatowata@gmail.com>
2022-08-01 12:45:32 -0300

    reqquisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,7 +2,17 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
 
+  const peopleName = data.employees
+    .find((e) => e.firstName === employeeName || e.lastName === employeeName);
+  return peopleName;
+
+  // if (employeeName) {
+  //   data.species.find((algo) => algo.name === 'lastName');
+  //   return employeeName;
+  // }
 }
-
 module.exports = getEmployeeByName;
