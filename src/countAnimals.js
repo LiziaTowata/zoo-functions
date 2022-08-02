147fd247f5f32fd1108a5eb8f4eb2bd9b415ddf3
Lizia Towata <liziatowata@gmail.com>
2022-08-02 16:58:22 -0300

    requisi 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,11 +1,20 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
-  if (species !== animal) {
-    // return onabort;
+  if (!animal) {
+    const pringles = {};
+    data.species.forEach((element) => {
+      pringles[element.name] = element.residents.length;
+    });
+    return pringles;
   }
+  if (!animal.sex) {
+    const caderno = data.species.find((element) => element.name === animal.specie);
+    return caderno.residents.length;
+  }
+  const caderno = data.species.find((element) => element.name === animal.specie);
+  return caderno.residents.filter((element) => element.sex === animal.sex).length;
 }
-
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 module.exports = countAnimals;
