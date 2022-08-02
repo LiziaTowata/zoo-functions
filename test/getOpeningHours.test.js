2b572925806e87fd3e399040d4d64a30b64c54e1
Lizia Towata <liziatowata@gmail.com>
2022-08-02 17:44:21 -0300

    requisito 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -2,7 +2,7 @@ const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {
   it('Teste não passando argumentos. Deverá retornar o objeto', () => {
-    expect(getOpeningHours()).toEqual({Tuesday: { open: 8, close: 6 },
+    expect(getOpeningHours()).toEqual({ Tuesday: { open: 8, close: 6 },
       Wednesday: { open: 8, close: 6 },
       Thursday: { open: 10, close: 8 },
       Friday: { open: 10, close: 8 },
@@ -23,5 +23,4 @@ describe('Testes da função getOpeningHours', () => {
   it('Para os argumentos Wednesdaye 09:00-PMdeve retornar uma string', () => {
     expect(getOpeningHours('Wednesday', '09:00-PM')).toEqual('The zoo is closed');
   });
-
 });
